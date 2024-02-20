20fc4769db42a9a347d9e571fa1f1dc73b515e40
andregeorge <ageorge96.ag@gmail.com>
2024-02-20 23:57:47 +0000

    add sign up functionality and navigation to signup page from welcome screen

--- MobileAcebook/Services/AuthenticationService.swift
+++ MobileAcebook/Services/AuthenticationService.swift
@@ -10,48 +10,30 @@ import Foundation
 class AuthenticationService: AuthenticationServiceProtocol {
     func signUp(user: User) -> Bool {
         guard let url = URL(string: "http://127.0.0.1:8080/users") else { return false }
-        print(user)
         var request = URLRequest(url:url)
-//        var payload = ["email": user.email, "username": user.username]
-//        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let payload = ["email": user.email, "username": user.username, "password": user.password, "avatar": user.avatar]
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
-        request.httpBody = "email=\(user.email)".data(using: .utf8)
+        
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: payload, options: [])
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding JSON: \(error)")
+            return false
+        }
         
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             guard let data = data, error == nil else {
                if let error = error {
                    print("Error: \(error)")
-               }
-            print(response)
-               return
+            }
+            print(response as Any)
+            return
            }
         }
         task.resume()
         return true // placeholder
     }
 }
-
-//guard let url = URL(string: "http://127.0.0.1:8080/posts") else { return }
-//        
-//        var request = URLRequest(url: url)
-//        request.httpMethod = "GET"
-//        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
-//        
-//        URLSession.shared.dataTask(with: request) { data, response, error in
-//            guard let data = data, error == nil else {
-//                if let error = error {
-//                    print("Error: \(error)")
-//                }
-//                return
-//            }
-//            
-//            do {
-//                let response = try JSONDecoder().decode(PostResponse.self, from: data)
-//                DispatchQueue.main.async {
-//                    self.posts = response.posts
-//                }
-//            } catch {
-//                print("Error decoding JSON: \(error)")
-//            }
-//        }.resume()
 
--- MobileAcebook/SignUpView.swift
+++ MobileAcebook/SignUpView.swift
@@ -11,11 +11,20 @@ import AuthenticationServices
 
 
 struct SignUpView: View {
+    
     @State var email = "";
     @State var username = "";
     @State var password = "";
-    @State var confirmPassword = ""
-    let authentication = AuthenticationService();
+    @State var confirmPassword = "";
+    
+        
+    @State private var invalidEmailMessage = false;
+    @State private var validUsername = true
+    @State private var passwordValidation = 0;
+    @State private var passwordMismatch = false;
+    
+    @StateObject private var authVM = AuthenticationViewModel()
+    private let authentication = AuthenticationService();
     
     var body: some View {
         
@@ -25,27 +34,109 @@ struct SignUpView: View {
             Form {
                 Section(header: Text("Email")){
                     TextField("Enter your email", text: $email)
+                        
+                    if invalidEmailMessage {
+                        Text("Email is invalid")
+                            .foregroundColor(Color.red)
+                            .multilineTextAlignment(.center)
+                            .frame(maxWidth: .infinity)
+                            .listRowBackground(Color(UIColor.systemGroupedBackground))
+                    }
                 }
+                
+                
+                
                 Section(header: Text("Username")){
                     TextField("Enter a username", text: $username)
+                    
+                    if !validUsername {
+                        Text("Please enter a username")
+                            .foregroundColor(Color.red)
+                            .multilineTextAlignment(.center)
+                            .frame(maxWidth: .infinity)
+                            .listRowBackground(Color(UIColor.systemGroupedBackground))
+                    }
                 }
                 Section(header: Text("Password")){
-                    TextField("Enter a password", text: $password)
+                    SecureField("Enter a password", text: $password)
+                    
+                    if passwordValidation == 1 {
+                        Text("Password must be longer than 8 chars")
+                            .foregroundColor(Color.red)
+                            .multilineTextAlignment(.center)
+                            .frame(maxWidth: .infinity)
+                            .listRowBackground(Color(UIColor.systemGroupedBackground))
+                    } else if passwordValidation == 2 {
+                        Text("Please include a lower case character, uppercase character and number")
+                            .foregroundColor(Color.red)
+                            .multilineTextAlignment(.center)
+                            .frame(maxWidth: .infinity)
+                            .listRowBackground(Color(UIColor.systemGroupedBackground))
+                    }
+                    
                 }
+                
                 Section(header: Text("Confirm Password")){
-                    TextField("Re-enter your password", text: $confirmPassword)
+                    SecureField("Repeat password", text: $confirmPassword)
+                    
+                    if passwordMismatch {
+                        Text("Passwords do not match")
+                            .foregroundColor(Color.red)
+                            .multilineTextAlignment(.center)
+                            .frame(maxWidth: .infinity)
+                            .listRowBackground(Color(UIColor.systemGroupedBackground))
+                    }
+                    
                 }
                 
                 Button("Sign up") {
-                    let user = User(email: email, username: username, password: password, avatar: "test.png")
+                    
+                    resetCheckers()
+                    
+                    if !isValidEmail(email: email) {
+                        invalidEmailMessage = true;
+                        return
+                    }
+                    
+                    if username == "" {
+                        validUsername = false
+                        return
+                    }
+                    
+                    passwordValidation = isValidPassword(password: password)
+                    if passwordValidation != 0 {
+                        return
+                    }
+                    
+                    if password != confirmPassword {
+                        passwordMismatch = true;
+                        return
+                    }
+                    
+                    let user = User(email: email, username: username, password: password, avatar: "placeholder.png")
                     authentication.signUp(user: user)
-                    email = "";
-                    username = "";
-                    password = "";
-                    confirmPassword = "";
+                    
+                    
+                    //replace with navigate to login
+                    resetStates()
                 }
             }
         }
+    }
+    
+    func resetCheckers() {
+        invalidEmailMessage = false
+        validUsername = true
+        passwordValidation = 0
+        passwordMismatch = false
+    }
+    
+    //delete once navigation to login is set
+    func resetStates() {
+        email = "";
+        username = "";
+        password = "";
+        confirmPassword = "";
     }
 }
 
