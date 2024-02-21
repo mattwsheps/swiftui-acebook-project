9c51232325a14f0b34d52996221cb777cab4f9e6
Matthew Gentle-Shepherd <matthew.gentle@gmail.com>
2024-02-21 14:47:03 +0000

    Like functionality added

--- MobileAcebook/Services/PostsService.swift
+++ MobileAcebook/Services/PostsService.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct getPostResponse: Codable {
     let posts: [Post]
-    let user: User?
+    let user: User
     let token: String
 }
 
@@ -18,12 +18,19 @@ struct getPostOwnerDataResponse: Codable {
     let token: String
 }
 
+struct LikeResponse: Decodable {
+    let message: String
+    let likes: Int
+}
+
 class PostsService: PostsServiceProtocol, ObservableObject {
     @Published var posts: [Post] = []
+    @Published var user: User?
+    @Published var likedStates: [String: Bool] = [:]
     private let baseUrlString = "http://127.0.0.1:8080"
     
     func getPosts(token: String) {
-        guard let url = URL(string: "http://127.0.0.1:8080/posts") else { return }
+        guard let url = URL(string: "\(baseUrlString)/posts") else { return }
         
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
@@ -41,12 +48,21 @@ class PostsService: PostsServiceProtocol, ObservableObject {
                 let response = try JSONDecoder().decode(getPostResponse.self, from: data)
                 DispatchQueue.main.async {
                     self.posts = response.posts
+                    self.user = response.user
+                    if let userID = self.user?.id {
+                        for post in self.posts {
+                            let isLiked = post.likes.contains(userID)
+                            self.likedStates[post.id] = isLiked
+                        }
+                    }
                     for post in self.posts {
                         self.getPostOwnerData(ownerID: post.createdBy ?? "", token: token) { owner in
                             if let owner = owner {
                                 // Update createdByUsername for the post
                                 if let index = self.posts.firstIndex(where: { $0.id == post.id }) {
                                     self.posts[index].createdByUsername = owner.username
+                                    self.posts[index].createdByAvatar = owner.avatar
+                                    self.posts[index].likesCount = self.posts[index].likes.count
                                 }
                             }
                         }
@@ -58,7 +74,7 @@ class PostsService: PostsServiceProtocol, ObservableObject {
         }.resume()
     }
     func getPostOwnerData(ownerID: String, token: String, completion: @escaping (User?) -> Void) {
-        guard let url = URL(string: "http://127.0.0.1:8080/posts/\(ownerID)") else { return }
+        guard let url = URL(string: "\(baseUrlString)/posts/\(ownerID)") else { return }
 
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
@@ -84,7 +100,6 @@ class PostsService: PostsServiceProtocol, ObservableObject {
             }
         }.resume()
     }
-    
     func createPost(token: String, message: String, image: String?, completion: @escaping (Bool, Error?) -> Void) {
             guard let url = URL(string: "\(baseUrlString)/posts") else { return } // if the baseurl is false return does not run the code
             var request = URLRequest(url: url)
@@ -117,5 +132,35 @@ class PostsService: PostsServiceProtocol, ObservableObject {
                 }
             }.resume()
         }
+    func likePost(token: String, postID: String, completion: @escaping (Int?) -> Void) {
+        guard let url = URL(string: "\(baseUrlString)/posts/\(postID)/like") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "PUT"
+        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                if let error = error {
+                    print("Error: \(error)")
+                }
+                completion(nil)
+                return
+            }
+
+            do {
+                let response = try JSONDecoder().decode(LikeResponse.self, from: data)
+                DispatchQueue.main.async {
+                    if let index = self.posts.firstIndex(where: { $0.id == postID }) {
+                        self.posts[index].likesCount = response.likes
+                    }
+                    completion(response.likes)
+                }
+            } catch {
+                print("Error decoding JSON: \(error)")
+                completion(nil)
+            }
+        }.resume()
+    }
 }
 
